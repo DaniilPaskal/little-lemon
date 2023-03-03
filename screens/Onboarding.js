2c2e3239ee485baf22f9b4cf9082aea61c94826a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-02 21:29:28 -0500

    Added stylesheet

--- screens/Onboarding.js
+++ screens/Onboarding.js
@@ -48,4 +48,57 @@ const Onboarding = ({ navigation }) => {
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        margin: 10,
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    regularText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        padding: 50,
+        marginVertical: 8,
+        textAlign: 'center',
+        color: 'black',
+    },
+    inputBox: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        borderColor: '#EDEFEE',
+        backgroundColor: '#EDEFEE',
+    },
+    buttonEnabled: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 20,
+        backgroundColor: '#495E57',
+        borderRadius: 10,
+    },
+    buttonDisabled: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 20,
+        backgroundColor: '#a3a5a8',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#EDEFEE',
+        textAlign: 'center',
+        fontSize: 18,
+    }
+})
+
 export default Onboarding;
