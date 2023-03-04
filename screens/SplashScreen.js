c64cfbd56a96cc3d09ae1e2f98a3c751776381a7
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-03 23:32:26 -0500

    Created SplashScreen

---
+++ screens/SplashScreen.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { View, Image, Text, Pressable, StyleSheet } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+    
+
+    return (
+    <View style={styles.container}>
+        
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        margin: 10,
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    regularText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        padding: 50,
+        marginVertical: 8,
+        textAlign: 'center',
+        color: 'black',
+    },
+    inputBox: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        borderColor: '#EDEFEE',
+        backgroundColor: '#EDEFEE',
+    },
+    buttonEnabled: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 20,
+        backgroundColor: '#495E57',
+        borderRadius: 10,
+    },
+    buttonDisabled: {
+        fontSize: 22,
+        padding: 10,
+        marginVertical: 8,
+        margin: 20,
+        backgroundColor: '#a3a5a8',
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: '#EDEFEE',
+        textAlign: 'center',
+        fontSize: 18,
+    }
+})
+
+export default SplashScreen;
