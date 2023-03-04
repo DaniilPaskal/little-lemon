259865e8f709fe242307de8527ef0a1c2f9d8c16
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-03-03 23:30:12 -0500

    Added stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Onboarding from './screens/Onboarding';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Onboarding />
+      <Stack.Navigator>
+        <Stack.Screen name='Onboarding' component={Onboarding} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
