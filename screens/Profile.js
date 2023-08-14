baf0e3f35ca533d58dbca682d1a9b25ca97559f6
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-08-13 22:25:19 -0400

    Added missing import

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { View, Image, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, Image, Text, TextInput, Pressable, StyleSheet, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { validateEmail } from '../utils';
 
