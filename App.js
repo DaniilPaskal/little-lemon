e94ec38f28933600cdfac2f2ceb521f7b69cab3a
DaniilPaskal <50429826+DaniilPaskal@users.noreply.github.com>
2023-09-28 03:06:40 -0400

    Set up conditional navigation

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer, useMemo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthContext } from './contexts/AuthContext';
 import Onboarding from './screens/Onboarding';
 import Profile from './screens/Profile';
 import Home from './screens/Home'
@@ -12,29 +13,96 @@ import SplashScreen from './screens/SplashScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [user, setUser] = useState({});
+  const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      if (action.type === 'LOG_IN') {
+        return {
+          ...prevState,
+          isLoading: false,
+          isLoggedIn: action.isLoggedIn
+        };
+      }
+    },
+    {
+      isLoading: true,
+      isLoggedIn: false
+    }
+  )
+  
+  const clearAsyncStorage = async() => {
+    AsyncStorage.clear();
+  }
 
   useEffect(() => {
     (async () => {
-        try {
-            const getUser = await AsyncStorage.getItem('user');
-            setUser(JSON.parse(getUser));
-        } catch (e) {
-            console.error();
+      var profile;
+
+      try {
+        const getUser = await AsyncStorage.getItem('user');
+        profile = getUser;
+      } catch (e) {
+          console.error(e);
+      } finally {
+        if (Object.keys(profile).length > 0) {
+          dispatch({ type: 'LOG_IN', isLoggedIn: true });
+        } else {
+          dispatch({ type: 'LOG_IN', isLoggedIn: false });
         }
+      }
     })();
-}, []);
+  }, []);
+
+  const authContext = useMemo(() => ({
+    logIn: async (data) => {
+      try {
+        const jsonData = JSON.stringify(data);
+        await AsyncStorage.setItem('user', jsonData);
+      } catch (e) {
+        console.error(e);
+      }
+
+      dispatch({ type: 'LOG_IN', isLoggedIn: true});
+    },
+    logOut: async () => {
+      try {
+        await AsyncStorage.clear();
+      } catch (e) {
+        console.error(e);
+      }
+
+      dispatch({ type: 'LOG_IN', isLoggedIn: false});
+    },
+    update: async (data) => {
+      try {
+        const jsonData = JSON.stringify(data);
+        await AsyncStorage.setItem('user', jsonData);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }), []);
+
+  if (state.isLoading) {
+    return (
+      <SplashScreen />
+    );
+  }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {!user.firstName &&
-          <Stack.Screen name='Onboarding' component={Onboarding} />
-        }
-          <Stack.Screen name='Home' component={Home} />
-          <Stack.Screen name='Profile' component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {state.isLoggedIn ? (
+            <>
+              <Stack.Screen name='Home' component={Home} />
+              <Stack.Screen name='Profile' component={Profile} />
+            </>
+          ) : (
+            <Stack.Screen name='Onboarding' component={Onboarding} />
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
